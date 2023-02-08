d110b694d0af5a4eafeef7ae263926fc11da5e1b
Hriteshsinha <98927609+Hriteshsinha@users.noreply.github.com>
2023-02-08 12:39:32 +0530

    klk
    lab 2

---
+++ Lab 2/lab2.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class lab2 {
+    public static void main(String[] args) {
+        System.out.println("Enter the user name: ");
+        Scanner obj = new Scanner(System.in);
+        String name1 = obj.nextLine();
+        System.out.println("Name1: "+ name1);
+        String name2 = obj.nextLine();
+        System.out.println("Name2: "+ name2);
+        String name3 = obj.nextLine();
+    
+        System.out.println("Name3: "+ name3);
+        String name4 = obj.nextLine();
+        System.out.println("Name4: "+ name4);
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+    }
+    
+}
